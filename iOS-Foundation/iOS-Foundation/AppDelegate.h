bd294bcea89736b0f577afa2dad0b49706484e1e
xzq666 <1192227411@qq.com>
2020-03-16 17:06:02 +0800

    First Commit

---
+++ iOS-Foundation/iOS-Foundation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS-Foundation
+//
+//  Created by qhzc-iMac-02 on 2020/3/16.
+//  Copyright © 2020 Xuzq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ iOS-Foundation/iOS-Foundation/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  iOS-Foundation
+//
+//  Created by qhzc-iMac-02 on 2020/3/16.
+//  Copyright © 2020 Xuzq. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
